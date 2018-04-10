505c2af1f0dfab699694cfa67e1e2e8b90723bf9
josiahgray12 <josiah.d.gray@vanderbilt.edu>
2018-04-10 16:05:36 -0500

    yuh

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-console.log("Parker and JD's day at the coffee shop.");
+console.log("Parker and JD's day at the coffee shop.")
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("We changed the color to green.");
